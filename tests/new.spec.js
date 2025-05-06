0d82dcbb7960cf96e1b878cd1e3d59464e2e5217
Surya Aethirajula <surya.aethirajula@realpage.com>
2025-05-06 17:24:07 +0530

    PW-1 - test

--- tests/new.spec.js
+++ tests/new.spec.js
@@ -5,7 +5,7 @@ test('has title', async ({ page }) => {
     await page.locator('#emailAddressInput').fill('admin:sabita.suggala@buildium.com');
     await page.locator('#passwordInput').fill('Buildium@123');
 
-    // await page.getByRole('button', { name: 'Sign in' }).waitFor({ state: 'visible' });
+    // await page.getByRole('button', { name: 'Sign ins' }).waitFor({ state: 'visible' });
     await page.getByRole('button', { name: 'Sign in', exact: true }).click();
     await page.waitForTimeout(30000);
     //await page.locator('text=Sign In').click
