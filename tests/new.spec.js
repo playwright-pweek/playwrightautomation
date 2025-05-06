5261f9be310c0c97f31ee0cb6c4b32f03730c6d7
Surya Aethirajula <surya.aethirajula@realpage.com>
2025-05-06 17:10:18 +0530

    first commit

---
+++ tests/new.spec.js
@@ -0,0 +1,15 @@
+import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+    await page.goto('https://sabithatesting1.buildiumstaging.com/manager/app/homepage/dashboard');
+    await page.locator('#emailAddressInput').fill('admin:sabita.suggala@buildium.com');
+    await page.locator('#passwordInput').fill('Buildium@123');
+
+    // await page.getByRole('button', { name: 'Sign in' }).waitFor({ state: 'visible' });
+    await page.getByRole('button', { name: 'Sign in', exact: true }).click();
+    await page.waitForTimeout(30000);
+    //await page.locator('text=Sign In').click
+
+    // Expect a title "to contain" a substring.
+    await expect(page).toHaveTitle("/homepage/dashboard");
+});
